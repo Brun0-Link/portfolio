99fa0dab48828a2764a86969c14a09ad878425bf
Bruno Alves <brunoalvestrajano250@gmail.com>
2024-01-06 23:52:24 -0300

    :sparkles: feat: início da seção 'sobre mim'

--- src/components/Sections/AboutSection.jsx
+++ src/components/Sections/AboutSection.jsx
@@ -1,7 +1,11 @@
 export function AboutSection() {
   return (
-    <section id="about" className='text-center'>
-      <h1 className='text-7xl text-red-600'>Sobre</h1>
+    <section id="about" className='h-[75%] p-12'>
+      <h1 className='text-7xl text-red-600'>Sobre mim</h1>
+      <article className="pt-8 text-3xl leading-relaxed">
+        <p className="mb-8">Do Rio de Janeiro, fiz graduação em Análise e Desenvolvimento de Sistemas por 4 anos mas me afastei da área por cerca de 2 anos. Em 2023 comecei a recuperar o tempo perdido, como nunca antes na vida percebi que a programação para web deveria ser meu tesouro e o que há de mais valioso que posso oferecer.</p>
+        <p>Meus objetivos hoje envolvem dominar 100% do Javascript, ser capaz de criar aplicações diversas para web com qualquer framework, aprimorar técnicas de design, performance, entre outras skill técnicas. Como objetivo médio pretendo aprimorar soft skills indispensáveis, como comunicação e criatividade. O objetivo maior é me tornar referência na área e me integrar a grandes nomes da tecnologia.</p>
+      </article>
     </section>
   )
 }
